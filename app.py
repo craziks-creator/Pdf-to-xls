47315f20872d112ab7084376bb7e816477253d46
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-08-13 14:01:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,48 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def extract_questions_and_options(pdf_path):
-    # Implement the same extract_questions_and_options function from the previous response
+    questions = []
+    options = []
+    pdf_document = fitz.open(pdf_path)
+    
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document[page_num]
+        text = page.get_text()
+        lines = text.split('\n')
+        
+        question = ""
+        option_group = []
+        is_question = True
+        
+        for line in lines:
+            stripped_line = line.strip()
+            if not stripped_line:
+                continue
+            
+            if is_question:
+                question += stripped_line + " "
+                is_question = False
+            else:
+                option_group.append(stripped_line)
+            
+            # Assuming that an empty line indicates the end of an option group
+            if not stripped_line:
+                questions.append(question)
+                options.append(option_group)
+                question = ""
+                option_group = []
+                is_question = True
+    
+    pdf_document.close()
+    return questions, options
 
 def save_to_excel(questions, options, output_excel_path):
-    # Implement the same save_to_excel function from the previous response
+    data = []
+    for i in range(len(questions)):
+        data.append({"Question": questions[i], "Options": options[i]})
+    
+    df = pd.DataFrame(data)
+    df.to_excel(output_excel_path, index=False)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -40,7 +78,7 @@ def index():
         questions, options = extract_questions_and_options(file_path)
 
         # Create Excel file
-        output_excel_path = "output_questions.xlsx"
+        output_excel_path = os.path.join(app.config["UPLOAD_FOLDER"], "output_questions.xlsx")
         save_to_excel(questions, options, output_excel_path)
 
         # Provide the Excel file for download
